1c6303a846c01f1653a708d8e1cc6fb16d7a32a5
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2018-01-25 07:55:37 -0500

    Dynamic nested routes example

---
+++ pages/dynamicNested/_category/_subCategory/_id.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div>Subcategory {{ $route.params.subCategory }}</div>
+    <div>Post {{ $route.params.id }}</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
