7bf30e175f37f2424aaa927297813a6999ae22a6
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2018-01-25 07:55:14 -0500

    Code cleanup

--- components/LangSwitcher.vue
+++ components/LangSwitcher.vue
@@ -10,15 +10,9 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-
 export default {
   name: 'LangSwitcher',
   computed: {
-    ...mapState('i18n', [
-      'locales',
-      'currentLocale'
-    ]),
     showLocales () {
       return this.$i18n.locales.filter(locale => locale.code !== this.$i18n.locale)
     }
